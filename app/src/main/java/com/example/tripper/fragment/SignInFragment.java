2b8a13cd4dd151afbd97305d33be8a6b3fa1e590
tszylkiewicz <43911837+tszylkiewicz@users.noreply.github.com>
2019-11-23 22:14:04 +0100

    [ADD] Navigation between sign in/up fragments

--- app/src/main/java/com/example/tripper/fragment/SignInFragment.java
+++ app/src/main/java/com/example/tripper/fragment/SignInFragment.java
@@ -7,10 +7,15 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.tripper.R;
 import com.example.tripper.viewmodel.SignInViewModel;
@@ -18,6 +23,11 @@ import com.example.tripper.viewmodel.SignInViewModel;
 public class SignInFragment extends Fragment {
 
     private SignInViewModel mViewModel;
+
+    private EditText email;
+    private EditText password;
+    private Button signIn;
+    private Button signUp;
 
     public static SignInFragment newInstance() {
         return new SignInFragment();
@@ -30,10 +40,25 @@ public class SignInFragment extends Fragment {
     }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(SignInViewModel.class);
-        // TODO: Use the ViewModel
-    }
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mViewModel = ViewModelProviders.of(requireActivity()).get(SignInViewModel.class);
 
+        email = view.findViewById(R.id.email);
+        password = view.findViewById(R.id.password);
+
+        signIn = view.findViewById(R.id.signIn);
+        signUp = view.findViewById(R.id.signUp);
+
+        final NavController navController = Navigation.findNavController(view);
+        signIn.setOnClickListener(view1 -> {
+            Toast.makeText(this.getContext(), "Sign in btn clicked", Toast.LENGTH_LONG).show();
+            navController.navigate(R.id.nav_map, null);
+        });
+
+        signUp.setOnClickListener(view1 -> {
+            Toast.makeText(this.getContext(), "Sign up btn clicked", Toast.LENGTH_LONG).show();
+            navController.navigate(R.id.signUpFragment, null);
+        });
+    }
 }
--- app/src/main/java/com/example/tripper/fragment/SignUpFragment.java
+++ app/src/main/java/com/example/tripper/fragment/SignUpFragment.java
@@ -7,17 +7,29 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.tripper.R;
+import com.example.tripper.viewmodel.SignInViewModel;
 import com.example.tripper.viewmodel.SignUpViewModel;
 
 public class SignUpFragment extends Fragment {
 
     private SignUpViewModel mViewModel;
+
+    private EditText username;
+    private EditText email;
+    private EditText password;
+    private EditText confirmPassword;
+    private Button signUp;
 
     public static SignUpFragment newInstance() {
         return new SignUpFragment();
@@ -29,11 +41,24 @@ public class SignUpFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_sign_up, container, false);
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(SignUpViewModel.class);
-        // TODO: Use the ViewModel
-    }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mViewModel = ViewModelProviders.of(requireActivity()).get(SignUpViewModel.class);
+
+        email = view.findViewById(R.id.username);
+        email = view.findViewById(R.id.email);
+        password = view.findViewById(R.id.password);
+        confirmPassword = view.findViewById(R.id.confirmPassword);
+
+        signUp = view.findViewById(R.id.signUp);
+
+        final NavController navController = Navigation.findNavController(view);
+
+        signUp.setOnClickListener(view1 -> {
+            Toast.makeText(this.getContext(), "Sign up btn clicked", Toast.LENGTH_LONG).show();
+            navController.navigate(R.id.nav_map, null);
+        });
+    }
 }
